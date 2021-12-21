f3b48ff73fd947ab8b060692cf341dd581ac9922
SimonBucko <bucino.36@gmail.com>
2021-12-21 02:48:02 +0100

    added js to first page

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,5 +1,6 @@
 const cprForm = document.getElementById("cprForm");
 cprForm.addEventListener("submit", (e) => {
     e.preventDefault()
-
+    sessionStorage.setItem("cpr", cprForm.cpr.value)
+    window.location.href = "/pages/selection"
 })
